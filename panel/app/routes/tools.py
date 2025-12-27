b3b8f0365458881ba30378b4dc869404360c0e84
KillHosein <your-email@example.com>
2025-12-27 04:47:11 +0330

    1

--- panel/app/routes/tools.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
-
-tools_bp = Blueprint('tools', __name__, url_prefix='/tools')
-
-@tools_bp.route('/')
-@login_required
-def index():
-    return render_template('pages/admin/tools.html')
